5f160505506efc5971dd716ab4381c508cf66280
Noorulhaq Rahimi <noorulhaqrahimi11@gmail.com>
2026-06-29 14:56:13 -0400

    edit

---
+++ src/data/contactInfo.jsx
@@ -0,0 +1,51 @@
+import LinkedIn, { GitHub, GMail, Telegram, Resume } from "../components/Icons";
+// import {  Discord, Twitter } from "../components/Icons";
+import NoorulhaqRahimiCV from "../assets/NoorulhaqRahimiCV.pdf"
+const contactInfo = [
+    {
+        id: 1,
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/in/dhawal793/",
+        icon: <LinkedIn />,
+    },
+    {
+        id: 2,
+        name: "GitHub",
+        link: "https://github.com/noorulhaqrahimi",
+        icon: <GitHub />,
+    },
+    {
+        id: 3,
+        name: "Mail",
+        link: "mailto:noorulhaqrahimi11@gmail.com",
+        icon: <GMail />,
+    },
+
+    {
+        id: 4,
+        name: "Telegram",
+        link: "https://t.me/Noorulhaq_Rahimi",
+        icon: <Telegram />,
+    },
+    {
+        id: 5,
+        name: "Resume",
+        link: NoorulhaqRahimiCV,
+        icon: <Resume />,
+        download: true
+    }
+    // {
+    //   id: 6,
+    //   name: "Twitter",
+    //   link: "twitter.com",
+    //   icon: <Twitter />,
+    // },
+    // {
+    //   id: 7,
+    //   name: "Discord",
+    //   link: "discord.com",
+    //   icon: <Discord />,
+    // },
+];
+
+export default contactInfo;
