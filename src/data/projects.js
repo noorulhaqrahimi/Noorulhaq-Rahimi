64667adbfda24b286e41add44d391ad2606e0135
Noorulhaq Rahimi <noorulhaqrahimi11@gmail.com>
2026-06-30 15:58:48 -0400

    comment the code

--- src/data/projects.js
+++ src/data/projects.js
@@ -80,66 +80,66 @@ const projects = [
             display: " hidden group-hover:flex"
         }
     },
-    {
-        img: FAC,
-        name: "FontAwesome Clone",
-        demo: "https://dhawal-793.github.io/Font-Awesome-Clone/",
-        code: "https://github.com/dhawal-793/Font-Awesome-Clone",
-        description: "FontAwesome Clone is a Project based on Fontawesome.com, created using React Js and Tailwind CSS.",
-        style: {
-            shadow: "shadow-[#FFD43B]",
-            cover: "from-[#FFD43B]",
-            display: " hidden group-hover:flex"
-        }
-    },
-    {
-        img: Simon_Game,
-        name: "Simon Game",
-        demo: "https://dhawal-793.github.io/Simon-Game/",
-        code: "https://github.com/dhawal-793/Simon-Game",
-        description: "Simon Game is a very simple game created using React Js and Tailwind CSS.",
-        style: {
-            shadow: "shadow-[#011F3F]",
-            cover: "from-[#011F3F]",
-            display: " hidden group-hover:flex"
-        }
-    },
-    {
-        img: Todo_App,
-        name: "TODO APP",
-        demo: "https://meen-to-do-app.onrender.com/",
-        code: "https://github.com/dhawal-793/MEEN-TODO-APP",
-        description: "MEEN ToDO App is a very simple Todo App created using MongoDb,Express Js EJs and Node Js.",
-        style: {
-            shadow: "shadow-[#A582E2]",
-            cover: "from-[#A582E2]",
-            display: " hidden group-hover:flex"
-        }
-    },
-    {
-        img: ECF,
-        name: "Ecommerce Frontend",
-        demo: "https://dhawal-793.github.io/E-Commerce-Website-Front-End-Design/",
-        code: "https://github.com/dhawal-793/E-Commerce-Website-Front-End-Design",
-        description: "Frontend Design of an Ecommerce Website created using HTML CSS JS and Bootstrap.",
-        style: {
-            shadow: "shadow-[#CEC3FB]",
-            cover: "from-[#CEC3FB]",
-            display: " hidden group-hover:flex"
-        }
-    },
-    {
-        img: CLI_TMA,
-        name: "Task Manager CLI",
-        demo: "https://github.com/dhawal-793/Command-line-application-for-Task-Management",
-        code: "https://github.com/dhawal-793/Command-line-application-for-Task-Management",
-        description: "This is a Task Management CLI App created using C++, here user can create, read, Update and delete tasks.",
-        style: {
-            shadow: "shadow-[#763145]",
-            cover: "from-[#763145]",
-            display: " hidden group-hover:flex"
-        }
-    },
+    // {
+    //     img: FAC,
+    //     name: "FontAwesome Clone",
+    //     demo: "https://dhawal-793.github.io/Font-Awesome-Clone/",
+    //     code: "https://github.com/dhawal-793/Font-Awesome-Clone",
+    //     description: "FontAwesome Clone is a Project based on Fontawesome.com, created using React Js and Tailwind CSS.",
+    //     style: {
+    //         shadow: "shadow-[#FFD43B]",
+    //         cover: "from-[#FFD43B]",
+    //         display: " hidden group-hover:flex"
+    //     }
+    // },
+    // {
+    //     img: Simon_Game,
+    //     name: "Simon Game",
+    //     demo: "https://dhawal-793.github.io/Simon-Game/",
+    //     code: "https://github.com/dhawal-793/Simon-Game",
+    //     description: "Simon Game is a very simple game created using React Js and Tailwind CSS.",
+    //     style: {
+    //         shadow: "shadow-[#011F3F]",
+    //         cover: "from-[#011F3F]",
+    //         display: " hidden group-hover:flex"
+    //     }
+    // },
+    // {
+    //     img: Todo_App,
+    //     name: "TODO APP",
+    //     demo: "https://meen-to-do-app.onrender.com/",
+    //     code: "https://github.com/dhawal-793/MEEN-TODO-APP",
+    //     description: "MEEN ToDO App is a very simple Todo App created using MongoDb,Express Js EJs and Node Js.",
+    //     style: {
+    //         shadow: "shadow-[#A582E2]",
+    //         cover: "from-[#A582E2]",
+    //         display: " hidden group-hover:flex"
+    //     }
+    // },
+    // {
+    //     img: ECF,
+    //     name: "Ecommerce Frontend",
+    //     demo: "https://dhawal-793.github.io/E-Commerce-Website-Front-End-Design/",
+    //     code: "https://github.com/dhawal-793/E-Commerce-Website-Front-End-Design",
+    //     description: "Frontend Design of an Ecommerce Website created using HTML CSS JS and Bootstrap.",
+    //     style: {
+    //         shadow: "shadow-[#CEC3FB]",
+    //         cover: "from-[#CEC3FB]",
+    //         display: " hidden group-hover:flex"
+    //     }
+    // },
+    // {
+    //     img: CLI_TMA,
+    //     name: "Task Manager CLI",
+    //     demo: "https://github.com/dhawal-793/Command-line-application-for-Task-Management",
+    //     code: "https://github.com/dhawal-793/Command-line-application-for-Task-Management",
+    //     description: "This is a Task Management CLI App created using C++, here user can create, read, Update and delete tasks.",
+    //     style: {
+    //         shadow: "shadow-[#763145]",
+    //         cover: "from-[#763145]",
+    //         display: " hidden group-hover:flex"
+    //     }
+    // },
     // {
     //     img: "",
     //     name: "WordCloud Generator",
