300d2da55c1ffc7b031f219027b368620e60671b
Noorulhaq Rahimi <noorulhaqrahimi11@gmail.com>
2026-07-02 02:41:58 -0400

    edit

--- src/data/contactInfo.jsx
+++ src/data/contactInfo.jsx
@@ -5,7 +5,7 @@ const contactInfo = [
     {
         id: 1,
         name: "LinkedIn",
-        link: "https://www.linkedin.com/in/dhawal793/",
+        link: "https://www.linkedin.com/in/noorulhaq-rahimi-843799397/",
         icon: <LinkedIn />,
     },
     {
@@ -33,13 +33,13 @@ const contactInfo = [
         link: NoorulhaqRahimiCV,
         icon: <Resume />,
         download: true
-    }
-    // {
-    //   id: 6,
-    //   name: "Twitter",
-    //   link: "twitter.com",
-    //   icon: <Twitter />,
-    // },
+    },
+    {
+      id: 6,
+      name: "Twitter",
+      link: "https://x.com/NoorulhaqRahim",
+      icon: <Twitter />,
+    },
     // {
     //   id: 7,
     //   name: "Discord",
